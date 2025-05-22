0d6739384905a679339353cbe4400e29af2c9f98
omi-otsuke <fortitude108@gmail.com>
2025-05-22 17:03:21 +0900

    ビルド用バッチファイルを作成

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+
+pushd "%~dp0"
+pyinstaller --noconsole --name UsageRecorder --add-data "conf\*.toml;conf" source\ura.py
+move dist\UsageRecorder\_internal\conf dist\UsageRecorder
+popd
+
+pause
